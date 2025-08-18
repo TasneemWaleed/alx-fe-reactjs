f87e06095ce1c0998cbf5ad6739e933822d235ad
Tasneem Waleed <tasneem1waleed@gmail.com>
2025-08-18 19:30:14 +0300

    configure routes in App.jsx to include AddRecipeForm, EditRecipeForm, and RecipeList

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBar from './components/SearchBar';
-import RecipeList from './components/RecipeList';
-import RecipeDetails from './components/RecipeDetails';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import EditRecipeForm from "./components/EditRecipeForm";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <Router>
-      <div style={{ maxWidth: '600px', margin: '0 auto', padding: '16px' }}>
-        <h1>Recipe Sharing App</h1>
+      <div>
+        <h1>Recipes</h1>
+        {/* Search Bar should always be visible */}
         <SearchBar />
+
         <Routes>
           <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/edit/:id" element={<EditRecipeForm />} />
         </Routes>
       </div>
     </Router>
