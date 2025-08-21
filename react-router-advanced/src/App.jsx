64f1d026abef0625067e43a4cf87dd35a2f7fa8a
Tasneem Waleed <tasneem1waleed@gmail.com>
2025-08-21 16:04:50 +0300

    fix

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,56 +1,36 @@
-import { Routes, Route, Link, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
-import BlogPost from "./pages/BlogPost";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import Post from "./components/Post";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-// Fake authentication
-const isAuthenticated = false; // change to true to simulate login
-
 function App() {
-  return (
-    <div>
-      {/* Navbar */}
-      <nav style={{ display: "flex", gap: "1rem", marginBottom: "20px" }}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/blog/123">Blog Example</Link>
-        <Link to="/login">Login</Link>
-      </nav>
+  const isAuthenticated = true; // change this to false to test protected route
 
-      {/* Routes */}
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
 
-        {/* Protected route */}
+        {/* Protected Profile Route with Nested Routes */}
         <Route
-          path="/profile"
+          path="/profile/*"
           element={
             <ProtectedRoute isAuthenticated={isAuthenticated}>
               <Profile />
             </ProtectedRoute>
           }
         >
-          {/* Nested routes */}
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
 
-        {/* Dynamic route */}
-        <Route path="/blog/:id" element={<BlogPost />} />
-
-        <Route path="/login" element={<Login />} />
-
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/" />} />
+        {/* Dynamic Route */}
+        <Route path="/post/:id" element={<Post />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
